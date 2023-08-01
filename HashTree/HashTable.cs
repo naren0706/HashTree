308a8699445f5e4a27a5df53f30e3261051b41c6
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-08-01 10:50:32 +0530

    [Narenthra Kishore] Add : new Solution for hash Table

---
+++ HashTree/HashTable.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashtree
+{
+    public class HashTable<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public HashTable(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in LinkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+    public struct KeyValue<k, v>
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+
+    }
+}
--- HashTree/Program.cs
+++ HashTree/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+namespace Hashtree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable<string, string> hash = new HashTable<string, string>(5);
+            hash.Add("0", "zero");
+            hash.Add("1", "one");
+            hash.Add("2", "two");
+            hash.Add("3", "three");
+            hash.Add("4", "four");
+            hash.Add("5", "five");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value : " + hash5);
+            hash.Remove("3");
+            string hash2 = hash.Get("3");
+            Console.WriteLine("2th index value : " + hash2);
+        }
+    }
+}
