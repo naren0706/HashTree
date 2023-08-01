850acc55b30f4b3d7c036567d57a2b6f83416297
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-08-01 16:06:11 +0530

    [Narenthra Kishore] add : new values and check using size operation

--- HashTree/Program.cs
+++ HashTree/Program.cs
@@ -11,6 +11,17 @@ namespace Hashtree
             BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
             binarySearchTree.Insert(30);
             binarySearchTree.Insert(70);
+            binarySearchTree.Insert(22);
+            binarySearchTree.Insert(40);
+            binarySearchTree.Insert(60);
+            binarySearchTree.Insert(95);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(16);
+            binarySearchTree.Insert(65);
+            binarySearchTree.Insert(63);
+            binarySearchTree.Insert(67);
+            binarySearchTree.GetSize();
             binarySearchTree.Display();
         }
     }
